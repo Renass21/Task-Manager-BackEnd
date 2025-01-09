de94c122440f2d2b676f0b2d4b864dbb13a91b96
renatodossantoss21 <renatodossantoss21@gmail.com>
2025-01-09 17:48:42 -0300

    feat: add index.js, npm, prettier and nodemon

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.status(200).send('Running project')
+app.get('/tasks', (req, res) => {
+    const tasks = [
+        {
+            description: 'Study programation',
+            stake: 'web development',
+            isCompleted: false,
+        },
+    ]
+    res.status(200).send(tasks)
 })
 
 app.listen(8000, () => console.log('Listening on port 8000'))
