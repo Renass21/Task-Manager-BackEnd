f1b72ffd2580e561c3c7407b60cbbcf5fb94b608
renass21 <renatodossantoss21@gmail.com>
2025-01-29 21:53:11 -0300

    fix: status code connection db

--- src/database/mongoose.database.js
+++ src/database/mongoose.database.js
@@ -5,9 +5,10 @@ const connectToDataBase = async () => {
         await mongoose
             .connect(process.env.DATABASE_URL)
             .then(console.log("MongoDB conectando..."));
-        console.log("MongoDB conectado!!!");
+        console.log();
+        return ("MongoDB conectado!!!", {status: 200});
     } catch (error) {
-        console.log("Erro ao conectar no MongoDB");
+       return ("Erro ao conectar no MongoDB", {status: 500});
     }
 };
 
