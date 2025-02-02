9fa9b2f0777f4e2a9146ddc7e09110becaa5c39a
renass21 <renatodossantoss21@gmail.com>
2025-02-02 14:11:34 -0300

    fix: refactor routes

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
-const dotenv = require("dotenv");
 const TaskRoutes = require("./src/routes/task.routes");
 
 const connectToDataBase = require("./src/database/mongoose.database");
 
-dotenv.config();
+
 const app = express();
 app.use(express.json());
 
