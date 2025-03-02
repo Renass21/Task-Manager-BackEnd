eed37d68ca36a617bc871944235b722f06713901
renass21 <renatodossantoss21@gmail.com>
2025-03-02 13:48:00 -0300

    fix: install cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const TaskRoutes = require("./src/routes/task.routes");
+const cors = require("cors");
 
 const connectToDataBase = require("./src/database/mongoose.database");
 
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 connectToDataBase();
